8313187917148fe4a2167395f1dba1bfd93b657d
Jeannie Fallon <jeanniefallon@gmail.com>
2017-05-11 17:25:01 -0400

    Add Map, List, and Set methods to model

--- src/main/java/org/launchcode/models/Bookshelf.java
+++ src/main/java/org/launchcode/models/Bookshelf.java
@@ -1,6 +1,6 @@
 package org.launchcode.models;
 
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * Created by jeannie on 5/10/17.
@@ -8,39 +8,74 @@ import java.util.HashMap;
 public class Bookshelf {
 
     private String name;
-    private HashMap<String, String> books;
+    private Map<String, String> books;
+    private List<String> titles;
+    private Set<String> authors;
 
+    // do I need a default constructor when not using a DAO?
     public Bookshelf() {
 
     }
 
+
     public Bookshelf(String name) {
         this.name = name;
-        this.books = new HashMap<String,String>();
+        this.books = new HashMap<>();           // use TreeMap for natural ordering
+        this.titles = new ArrayList<>();        // use List to allow for duplicate title entries
+        this.authors = new TreeSet<>();         // use Set to prevent duplicate author entries
     }
+
 
     public String getName() {
         return name;
     }
 
+
     public void setName(String name) {
         this.name = name;
     }
 
-    public HashMap<String, String> getBooks() {
+
+    public Map<String, String> getBooks() {
         return books;
     }
 
-    public void setBooks(HashMap<String, String> books) {
-        this.books = books;
+
+    public List<String> getTitles() {
+        return titles;
     }
+
+
+    public Set<String> getAuthors() {
+        return authors;
+    }
+
 
     public void addBook(String title, String author) {
         books.put(title, author);
     }
 
+
     public void removeBook(String title) {
         books.remove(title);
+    }
+
+
+    // iteratre over Map's keyset and assign keys to List of titles
+    public List<String> findTitles() {
+        for(String title : books.keySet()) {
+            titles.add(title);
+        }
+        return titles;
+    }
+
+
+    // iterature over Map's values and assign values to Set of authors
+    public Set<String> findAuthors() {
+        for(String author: books.values()) {
+            authors.add(author);
+        }
+        return authors;
     }
 
 }

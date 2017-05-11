e5dab0b305b5f9da3848230c5dbe779f5e1996a8
Jeannie Fallon <jeanniefallon@gmail.com>
2017-05-11 17:00:24 -0400

    Stub out templates and handlers

--- src/main/java/org/launchcode/controllers/BookshelfController.java
+++ src/main/java/org/launchcode/controllers/BookshelfController.java
@@ -20,6 +20,42 @@ public class BookshelfController {
         fiction.addBook("Brave New World", "Aldous Huxley");
         fiction.addBook("The Great Gatsby", "F. Scott Fitzgerald");
         model.addAttribute("bookshelf", fiction);
+        model.addAttribute("title", "Books");
         return "bookshelf/index";
     }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute("title","Add a Book");
+        return "bookshelf/add";
+    }
+
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String remove(Model model) {
+        model.addAttribute("title","Remove a Book");
+        return "bookshelf/remove";
+    }
+
+
+    @RequestMapping(value = "authors", method = RequestMethod.GET)
+    public String authors(Model model) {
+        model.addAttribute("title","Authors");
+        return "bookshelf/authors";
+    }
+
+
+    @RequestMapping(value = "titles", method = RequestMethod.GET)
+    public String titles(Model model) {
+        model.addAttribute("title","Titles");
+        return "bookshelf/titles";
+    }
+
+
+    @RequestMapping(value = "about", method = RequestMethod.GET)
+    public String about(Model model) {
+        model.addAttribute("title","About");
+        return "bookshelf/about";
+    }
+
 }
